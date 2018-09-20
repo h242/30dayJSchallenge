8ff014c774aa9c4b2e877689c63cc1bf3639761c
jimena <jimena.piano@globallogic.com>
2018-09-20 00:49:06 -0300

    greenScreen IS ALIIIVE

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -28,13 +28,13 @@ function paintToCanvas() {
 		let pixels = ctx.getImageData(0, 0, width, height);
 		// mess with them
 		//pixels = redEffect(pixels);
-		// pixels = rgbSplit(pixels);
-		// ctx.globalAlpha = 0.1;
+		pixels = rgbSplit(pixels);
+		ctx.globalAlpha = 0.3;
 		pixels = greenScreen(pixels);
 		// put them back
 		ctx.putImageData(pixels, 0, 0);
 
-	}, 16);
+	}, 100);
 }
 
 function takePhoto() {
@@ -48,12 +48,12 @@ function takePhoto() {
 	link.href = data;
 	link.setAttribute('download', 'handsome');
 	//link.textContent = 'Download Image';
-	link.innerHTML = `<img src="${data}" alt="Handsome Grrrl />`;
+	link.innerHTML = `<img src="${data}" alt="HandsomeGrrrl"/>`;
 	strip.insertBefore(link, strip.firstChild);
 }
 
 function redEffect(pixels) {
-	for(let i = 0; i < pixels.data.length; i+=4) {
+	for (let i = 0; i < pixels.data.length; i += 4) {
 		pixels.data[i + 0] = pixels.data[i + 0] + 100; //R
 		pixels.data[i + 1] = pixels.data[i + 1] - 50; //G
 		pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //B
@@ -62,7 +62,7 @@ function redEffect(pixels) {
 }
 
 function rgbSplit(pixels) {
-	for(let i = 0; i < pixels.data.length; i+=4) {
+	for (let i = 0; i < pixels.data.length; i += 4) {
 		pixels.data[i - 150] = pixels.data[i + 0] + 100; //R
 		pixels.data[i + 100] = pixels.data[i + 1] - 50; //G
 		pixels.data[i - 550] = pixels.data[i + 2] * 0.5; //B
@@ -73,22 +73,19 @@ function rgbSplit(pixels) {
 function greenScreen(pixels) {
 	const levels = {};
 
-	document.querySelectorAll('.rgb input').forEach((input) => {
+	document.querySelectorAll('.rgb input').forEach(input => {
 		levels[input.name] = input.value;
 	});
 
-	for (i = 0; i < pixels.data.length; i = i + 4) {
+	for (i = 0; i < pixels.data.length; i += 4) {
 		red = pixels.data[i + 0];
 		green = pixels.data[i + 1];
 		blue = pixels.data[i + 2];
-		alpha = pixels.data[i + 3];
 
-		if (red >= levels.rmin
-			&& green >= levels.gmin
-			&& blue >= levels.bmin
-			&& red >= levels.rmax
-			&& green >= levels.gmax
-			&& blue >= levels.bmax) {
+		if (levels.rmin <=   red && red   <= levels.rmax
+		&&  levels.gmin <= green && green <= levels.gmax
+		&&  levels.bmin <=  blue && blue  <= levels.bmax
+		) {
 			pixels.data[i + 3] = 0;
 		}
 	}
